c5c171e0853d802c6eb5d3353a12f4297e3d2269
shynome <shynome@gmail.com>
2019-12-15 14:51:33 +0800

    udpate

--- libs/browser/api-client.ts
+++ libs/browser/api-client.ts
@@ -1,4 +1,16 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { useMemo } from "react";
+import { Config } from "~libs/caddy/Config";
+
+export const enum Action {
+  /**对象是数组的话追加, 对象的话创建或替换 */
+  POST = 'post',
+  /**对象是数组的话追加, 对象的话创建 */
+  PUT = 'put',
+  /**替换 */
+  PATCH = 'patch',
+  DELETE = 'delete',
+}
 
 export const servers = axios.create({
   baseURL: '/caddy2-api/config/apps/http/servers'
@@ -8,3 +20,33 @@ export const api = axios.create({
   baseURL: '/caddy2-api/config'
 })
 
+import { caddy2Config } from "./caddy2";
+export const useUpdateConfig = (scopeInstace: AxiosInstance) => {
+  const [config, setConfig] = caddy2Config.useContainer()
+  const updateServer = useMemo(() => {
+    return async (
+      action: Action,
+      path: string,
+      dispath: (dispath: typeof setConfig, config: Config, data: any, ) => void | Promise<void>,
+      data?: any,
+    ) => {
+      switch (action) {
+        case Action.POST:
+          await scopeInstace.post(path, data)
+          break;
+        case Action.PUT:
+          await scopeInstace.put(path, data)
+          break;
+        case Action.PATCH:
+          await scopeInstace.patch(path, data)
+          break;
+        case Action.DELETE:
+          await scopeInstace.delete(path)
+          break;
+      }
+      let d = await scopeInstace.get(path).then(r => r.data)
+      await dispath(setConfig, config, d)
+    }
+  }, [config, setConfig])
+  return updateServer
+}
--- pages/server/index.ts
+++ pages/server/index.ts
@@ -4,3 +4,9 @@ import { getServers, Server as _Server } from "~pages/servers";
 export type Server = _Server
 
 export const getServer = (config: Config, which: string) => getServers(config)[which]
+
+import { servers, useUpdateConfig } from "~libs/browser/api-client";
+
+export const useServerUpdate = () => {
+  return useUpdateConfig(servers)
+}
