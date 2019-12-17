1b9694bfbd6521e7464c434486dd4ffc4638f55d
shynome <shynome@gmail.com>
2019-12-17 22:06:31 +0800

    fix open

--- pages/editor/EditorDialog/useEditor.ts
+++ pages/editor/EditorDialog/useEditor.ts
@@ -7,11 +7,12 @@ export const useEditor = () => {
     let nextState = ((state) => (state.open = false, state))(copy(state))
     setState(nextState)
   }
-  const open = ({ config, file }: { config: string, file: string }, onSave: (config: string) => Promise<any>) => {
+  const open = ({ config, file }: { config: string, file: string }, onSave: (config: string) => Promise<any> | any) => {
     let nextState = ((state) => (
       state.file = file,
       state.value = config,
       state.onSave = onSave,
+      state.open = true,
       state
     ))(copy(state))
     setState(nextState)
