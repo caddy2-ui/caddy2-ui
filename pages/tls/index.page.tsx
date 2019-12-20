c3b9fe7d8c88f67f9342913821f366582c20639f
shynome <shynome@gmail.com>
2019-12-20 21:43:46 +0800

    archive

--- pages/tls/index.page.tsx
+++ pages/tls/index.page.tsx
@@ -3,12 +3,25 @@ import { caddy2Config } from "~libs/browser/caddy2";
 import { makeStyles, useTheme, colors } from "@material-ui/core";
 import { Main as MainLayout } from "~pages/layouts";
 import {
-  Container,
   Grid,
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
 } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 import { CertList, Props } from "./components/CertList";
 import { Certs, getCerts } from "./index";
-
+import { ContentLayout } from "./components/ContentLayout";
+import { CertInfo, parseCert } from "~pages/server/tls_connection/CerInfo";
+import { useEditor } from "~pages/editor";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -38,32 +51,46 @@ const MemCertColumn = [
   createColumn('标签', (d) => d.tags),
 ]
 
-export default () => {
+const MemPage = () => {
 
   const classes = useStyles(useTheme())
+  const editor = useEditor()
   const [config] = caddy2Config.useContainer()
   const certs = getCerts(config)
+  let memCerts = certs.pem_loader || []
+
+  const openEditor = () => {
+    editor.open(
+      {
+        file: '',
+        config: {},
+      },
+      () => {
+
+      }
+    )
+  }
 
   return (
-    <MainLayout>
-      <Container className={classes.root} maxWidth='lg'>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <CertList
-              title="本地证书"
-              columns={fileCertColumn}
-              data={certs.load_files || []}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <CertList
-              title="内存证书"
-              columns={MemCertColumn}
-              data={certs.pem_loader || []}
-            />
-          </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <Button onClick={openEditor} startIcon={<AddIcon />} variant='outlined'>新增</Button>
+      </Grid>
+      {memCerts.map((data) => (
+        <Grid item key={data.certificate}
+          lg={4}
+          md={6}
+          xl={3}
+          xs={12}
+        >
+          <CertInfo certificate={parseCert(data.certificate)}></CertInfo>
         </Grid>
-      </Container>
-    </MainLayout>
+      ))}
+    </Grid>
   )
 }
+export default () => (
+  <ContentLayout>
+    <MemPage />
+  </ContentLayout>
+)
